5c642590b2c3f2f843d434680e81c3bed89bc4f9
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-08-10 23:36:54 +0700

    Hello World

---
+++ bin/dart_id_networkers.dart
@@ -0,0 +1,5 @@
+import 'package:dart_id_networkers/dart_id_networkers.dart' as dart_id_networkers;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_id_networkers.calculate()}!');
+}
---
+++ test/dart_id_networkers_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_id_networkers/dart_id_networkers.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
